9ca10bbbf5fccc35e6ac00de898c709768b44638
mezanix <mezanixissa@gmail.com>
2026-07-14 19:00:01 +0200

    Player Controller and Enhanced Inputs setup

---
+++ Private/Player/ShooterPlayerController.cpp
@@ -0,0 +1,87 @@
+﻿// Copyrights Mazen Issa - neckkeys
+
+
+#include "Player/ShooterPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AShooterPlayerController::AShooterPlayerController()
+{
+	bReplicates = true;
+}
+
+void AShooterPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		Subsystem->AddMappingContext(ShooterIMC, 0);
+	}
+}
+
+void AShooterPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* ShooterInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	ShooterInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::InputMove);
+	ShooterInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::InputLook);
+	ShooterInputComponent->BindAction(CrouchAction, ETriggerEvent::Started, this, &ThisClass::InputCrouch);
+	ShooterInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::InputJump);
+}
+
+void AShooterPlayerController::InputCrouch()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	if (UCharacterMovementComponent* CMC = GetCharacter()->GetCharacterMovement(); IsValid(CMC))
+	{
+		CMC->bWantsToCrouch = !CMC->bWantsToCrouch;
+	}
+}
+
+void AShooterPlayerController::InputJump()
+{
+	
+	if (!IsValid(GetCharacter())) return;
+	UCharacterMovementComponent* CMC = GetCharacter()->GetCharacterMovement();
+	if (!IsValid(CMC)) return;
+	
+	if (CMC->bWantsToCrouch)
+	{
+		CMC->bWantsToCrouch = false;
+	}
+	else
+	{
+		GetCharacter()->Jump();
+	}
+}
+
+void AShooterPlayerController::InputLook(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	AddYawInput(InputAxisVector.X);
+	AddPitchInput(InputAxisVector.Y);
+}
+
+void AShooterPlayerController::InputMove(const FInputActionValue& InputActionValue)
+{
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+	
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	if (APawn* ControlledPawn = GetPawn())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
---
+++ Public/Player/ShooterPlayerController.h
@@ -0,0 +1,47 @@
+﻿// Copyrights Mazen Issa - neckkeys
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShooterPlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERFPS_API AShooterPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AShooterPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
+private:
+	UPROPERTY(EditAnywhere, Category="FPS|Input")
+	TObjectPtr<UInputMappingContext> ShooterIMC;
+	
+	UPROPERTY(EditAnywhere, Category="FPS|Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditAnywhere, Category="FPS|Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(EditAnywhere, Category="FPS|Input")
+	TObjectPtr<UInputAction> CrouchAction;
+	
+	UPROPERTY(EditAnywhere, Category="FPS|Input")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	void InputCrouch();
+	void InputJump();
+	void InputLook(const FInputActionValue& InputActionValue);
+	void InputMove(const FInputActionValue& InputActionValue);
+};
