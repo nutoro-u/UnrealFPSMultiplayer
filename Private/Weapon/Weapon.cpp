9bf7435d6d9899f706f74b6b92f9aa047b569c65
mezanix <mezanixissa@gmail.com>
2026-07-15 08:31:55 +0200

    Creating Weapon class

---
+++ Private/Weapon/Weapon.cpp
@@ -0,0 +1,30 @@
+﻿// Copyrights Mazen Issa - neckkeys
+
+
+#include "Weapon/Weapon.h"
+
+#include "Components/SkeletalMeshComponent.h"
+
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh1P");
+	Mesh1P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh1P->bReceivesDecals = false;
+	Mesh1P->CastShadow = false;
+	Mesh1P->SetHiddenInGame(true);
+	SetRootComponent(Mesh1P);
+	
+	Mesh3P = CreateDefaultSubobject<USkeletalMeshComponent>("Mesh3P");
+	Mesh3P->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::OnlyTickPoseWhenRendered;
+	Mesh3P->bReceivesDecals = false;
+	Mesh3P->CastShadow = false;
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Public/Weapon/Weapon.h
@@ -0,0 +1,26 @@
+﻿// Copyrights Mazen Issa - neckkeys
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class MULTIPLAYERFPS_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh1P;
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh3P;
+};
